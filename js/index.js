40b70e5cb2497f1c2b49a0ee07a3f1c5a830f9c8
Franco Marchese <franco.marchese@sulz.cl>
2023-05-25 16:10:15 -0400

    cambios en vista mobil y js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,71 @@
-const images = [
-    "img/hero/hero a.png",
-    "img/hero/hero b.png",
-]
+window.onload = (e => {
+    const stats = [
+        {
+            "dt": "Investigadores Asociados",
+            "dd": "7"
+        },
+        {
+            "dt": "Asistentes de Investigación",
+            "dd": "5"
+        },
+        {
+            "dt": "Investigadores Externos Nacionales",
+            "dd": "12"
+        },
+        {
+            "dt": "Investigadores Externos Internacionales",
+            "dd": "8"
+        },
+        {
+            "dt": "Publicaciones Indexadas",
+            "dd": "+20"
+        },
+        {
+            "dt": "Conferencias / Congresos",
+            "dd": "8"
+        },
+        {
+            "dt": "Memoristas UDLA",
+            "dd": "2"
+        },
+        {
+            "dt": "Practicantes UDLA",
+            "dd": "+10"
+        },
+        {
+            "dt": "Vinculación con el Medio",
+            "dd": "4"
+        },
+        {
+            "dt": "Proyectos con Fondos Internos adjudicación",
+            "dd": "3"
+        },
+        {
+            "dt": "Co-tutorias de Memorias otras universidades",
+            "dd": "5"
+        },
+        {
+            "dt": "Participación en programas de Postgrado UDLA",
+            "dd": "2"
+        },
+    ];
 
-const img = document.createElement("img");
-img.src = images[0];
-const container = document.getElementById("image-ctn");
-container.appendChild(img)
+    console.log("first flag")
+
+    for (let i = 0; i < stats.length; i++) {
+        let container = document.getElementsByTagName("dl");
+        let el = `
+            <div class="flex flex-col bg-gray-400/5 p-8 shadow-lg place-content-center snap-center">
+            <dt class="text-lg font-semibold leading-6 text-gray-600">${stats[i].dt}</dt>
+            <dd class="order-first text-3xl font-semibold tracking-tight text-orange-600">${stats[i].dd}</dd>
+            </div>
+        `;
+        let content = [];
+
+        content.push(el);
+        container.innerHTML = el
+        console.log("done the " + i)
+      };
+
+    console.log("second flag")
+});
